c705df5e43eb09db20033aee1f66a37fa91e830d
Sarzhin Ivan <4228815@mail.ru>
2022-04-15 14:26:47 +0300

    hw4 fix storageOverflow

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -5,6 +5,7 @@ import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -96,8 +97,13 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = StorageException.class)
     public void storageOverflow() throws Exception {
-        for (int i = 0; i <= storage.size(); i++) {
-            storage.save(new Resume());
+        try {
+            for (int i = 4; i <= AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            Assert.fail();
         }
+        storage.save(new Resume());
     }
 }
