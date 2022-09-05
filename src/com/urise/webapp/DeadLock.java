e6712c4a89c4a04c161602f3ea827f852a66fca0
IPS19 <i89214228815@gmail.com>
2022-09-05 20:30:39 +0300

    hw 11

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -1,35 +1,35 @@
 package com.urise.webapp;
 
 public class DeadLock {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         final Object resource1 = "resource1";
         final Object resource2 = "resource2";
         Thread t1 = new Thread() {
             public void run() {
-                synchronized(resource1){
+                synchronized (resource1) {
                     System.out.println("Thread t1: locked resource 1");
                     try {
                         sleep(20);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    synchronized(resource2){
+                    synchronized (resource2) {
                         System.out.println("Thread t1: locked resource 2");
                     }
                 }
             }
         };
 
-        Thread t2 = new Thread(){
-            public void run(){
-                synchronized(resource2){
+        Thread t2 = new Thread() {
+            public void run() {
+                synchronized (resource2) {
                     System.out.println("Thread t2: locked resource 2");
                     try {
                         sleep(20);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    synchronized(resource1){
+                    synchronized (resource1) {
                         System.out.println("Thread t2: locked resource 1");
                     }
                 }
