5074c46ce31ac3d02a5bb763d7c9abbd733ceeab
IPS19 <i89214228815@gmail.com>
2022-09-19 14:33:57 +0300

    hw 12

--- src/com/urise/webapp/StreamApi.java
+++ src/com/urise/webapp/StreamApi.java
@@ -7,20 +7,37 @@ import java.util.stream.Collectors;
 public class StreamApi {
     public static void main(String[] args) {
         int arr[] = {1, 5, 3, 3, 2, 3};
-        minValue(arr);
+        System.out.println(minValue(arr));
         System.out.println(oddOrEven(List.of(10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 111, 116)));
     }
 
     static int minValue(int[] values) {
-        int result = Arrays.stream(values).distinct().sorted().reduce(0, (a, b) -> (a * 10) + b);
-        System.out.println(result);
-        return result;
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (a, b) -> (a * 10) + b);
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
-        if (integers.stream().reduce((a, b) -> a + b).stream().anyMatch(x -> x % 2 == 0)) {
-            return integers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
-        }
-        return integers.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
+
+/*        boolean isOdd = integers
+                .stream()
+                .reduce((a, b) -> a + b)
+                .stream()
+                .anyMatch(x -> x % 2 == 0);
+
+        return integers
+                .stream()
+                .filter(x -> isOdd ? (x % 2 == 0) : (x % 2 != 0))
+                .collect(Collectors.toList());*/
+
+        return integers
+                .stream()
+                .filter(x -> (integers
+                        .stream()
+                        .reduce((a, b) -> a + b)
+                        .stream()
+                        .anyMatch(y -> y % 2 == 0)) ? (x % 2 == 0) : (x % 2 != 0))
+                .collect(Collectors.toList());
     }
 }
